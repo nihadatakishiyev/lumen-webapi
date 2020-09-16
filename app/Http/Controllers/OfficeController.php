8da621b9b01a19bccd52189ab02309744fbf1eef
nihad <n.atakishiyev@asan.gov.az>
2020-09-16 17:47:01 +0400

    resolves api connectivity issue

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Office;
+use Illuminate\Support\Facades\DB;
 
 class OfficeController extends Controller
 {
@@ -13,7 +14,16 @@ class OfficeController extends Controller
      */
     public function index()
     {
-        return app('db')->select("SELECT * FROM classicmodels.offices");
+        $tt = DB::select("select top 5 su.consumer_id, c.name, sum(su.cost) as total_cost
+                from rel.ServiceUsage su
+                left join list.Consumers c on su.consumer_id = c.id
+                where cast(su.create_date as date) = cast(getdate() as date)
+                and service_usage_status_id = 1
+                group by consumer_id, c.name
+                order by total_cost desc");
+//        dd($tt);
+        return response()->json($tt);
+//        return response()->json('test');
     }
 
 
